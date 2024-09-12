c13215dfa4faaa1e21e3a589f84ebac606f524c6
Muhammad Mubashir <mubashirbehlum606@gmail.com>
2024-09-12 19:34:52 +0500

    Add files via upload

---
+++ 1 to 7/index.js
@@ -0,0 +1,185 @@
+//! Chapter 1
+// Q.1
+//! ALERT
+// alert("Hello! World");
+
+// Chapter 2
+// Q.1
+// VARIABLE FOR STRINGS
+// var username;
+
+// Chapter 3
+// Q.1
+// VARIABLE FOR NUMBERS
+// var age = "I am 15 years old";
+// alert(age);
+
+//! Chapter 4
+//Q.1
+// var name1 = "variable1",
+//   name2 = "variable2",
+//   name3 = "variable3";
+
+// //Q.2
+// // Declare 5 legal variable names
+// var $myVar = "legal1";
+// var _myVar = "legal2";
+// var myVar3 = "legal3";
+// var my_Var4 = "legal4";
+// var my$Var5 = "legal5";
+// // Declare 5 illegal variable names
+// // var 1stVar = "illegal1";
+// // var my-Var = "illegal2";
+// // var var name = "illegal3";
+// // var var = "illegal4";
+// // var @myVar = "illegal5";
+
+// //Q.3
+// //(a)
+// var names1 = "Rules for naming JS Variable";
+// console.log(names1);
+// // (b)
+// //Letters
+// //Digits
+// //Underscores
+// //No space
+// var names2= "Letters"  + "Digits" + "Underscores" + "No space";
+// console.log(names2);
+
+// //(c)
+// //Letter
+// //Digit
+// //Underscore
+// var names3= "Letter" + "Digit" + "Underscore";
+// console.log(names3);
+// //(d)
+// //Variable are case sensitive.
+// var names4 = "Variable are case sensitive.";
+// console.log(names4);
+
+// //(e)
+// // Variable names should not be JavaScript reserved keywords.
+// var names5 = "Variable names should not be JavaScript reserved keywords."
+// console.log(names5);
+
+//!chapter 5
+
+// Q.1
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// // Q.2
+// // Subtraction
+// var difference = num1 - num2;
+// console.log(difference);
+// // Multiplication
+// var product = num1 * num2;
+// console.log(product);
+// // Division
+// var quotient = num1 / num2;
+// console.log(quotient);
+// // Modulus
+// var remainder = num1 % num2;
+// console.log(remainder);
+
+// // Q.3
+// // a. Declare a variable
+// var myVariable;
+// // b. Show the value of the variable after declaration
+// console.log(myVariable);
+// // c. Initialize the variable with some number
+// myVariable = 5;
+// // d. Show the initial value of the variable
+// console.log(myVariable);
+// // e. Increment the variable
+// myVariable++;
+// // f. Show the value after increment
+// console.log(myVariable);
+// // g. Add 7 to the variable
+// console.log(myVariable += 7);
+// // h. Show the final value of the variable
+// console.log(myVariable +=13);
+// //i.decrement the variable
+// var myVariable = 13;
+// console.log( --myVariable);
+// //show the remainder after dividing the variable's Value by 3.
+// var myVariable1 = 0;
+// var myVariable2 = 3;
+// console.log(myVariable1 / myVariable2);
+
+// var table = 4;
+// var table1 = 1;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 2;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 3;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 4;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 5;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 6;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 7;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 7;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 8;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 9;
+// console.log(table * table1);
+// var table = 4;
+// var table1 = 10;
+// console.log(table * table1);
+
+// chapter 6
+//Q.1
+
+// // Initialize the variable
+// var a = 10;
+
+// // Display the result in the browser
+// document.write("Result:<br>");
+// document.write("The value of a is:<br> " + a);
+// // Perform ++a (pre-increment)
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a++ (post-increment)
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform --a (pre-decrement)
+// document.write("The value of --a is: " + (--a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a-- (post-decrement)
+// document.write("The value of a-- is: " + (a--) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+//Q.2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("Result: " + result);
+
+// Chapter 7
+// var a = 10;
+// // Pre-increment
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// // Post-increment
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// // Pre-decrement
+// document.write("The value of --a is: " + (--a) + "<br>");
+// // Post-decrement
+// document.write("The value of a-- is: " + (a--) + "<br>");
